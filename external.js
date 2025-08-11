0f46be49762207497d9a8c72bbedc54ddeb610f1
Manezha Ebrahimi <manizhaebrahimi8@gmail.com>
2025-08-11 17:07:38 +0430

    Add external.js & index.html
    I Just linked JS to HTML.

---
+++ external.js
@@ -0,0 +1,19 @@
+console.log("JavaScript file loaded successfully.");
+var externalVariable = "This is an external variable.";
+console.log(externalVariable)
+let stu_name = "Manezha";
+let age = "17"
+console.log(`Name: ${stu_name}`);
+console.log(`Age: ${age}`);
+const PI = 3.14;
+console.log(`Value of PI: ${PI}`);
+age = 18;
+console.log(`Name: ${stu_name}`);
+console.log(`Age: ${age}`)
+function greet() {
+    let City = "Kabul";
+    console.log(`City: ${City}`);
+    console.log("Hello form the external JavaScript file function!!!!");
+    console.log(`Name...: ${stu_name}, Age...: ${age}`);
+}
+greet();
